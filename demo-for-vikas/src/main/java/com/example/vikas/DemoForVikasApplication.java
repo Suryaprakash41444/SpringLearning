268dc3d40fb2e74f7f30f09ba3e9ca7085267a97
surya prakash <surya@gmail.com>
2024-09-11 12:31:25 +0530

    added spring boot project

---
+++ demo-for-vikas/src/main/java/com/example/vikas/DemoForVikasApplication.java
@@ -0,0 +1,13 @@
+package com.example.vikas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForVikasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForVikasApplication.class, args);
+	}
+
+}
